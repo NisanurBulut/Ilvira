b1c40c19d6b83fe4ad7a0f15ea5876f7c0d22cb3
NisanurBulut <nbulut@sarplojistik.com>
2021-04-18 14:47:43 +0300

    implemented orderdetail model

---
+++ IlviraAPI/Models/OrderDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IlviraAPI.Models
+{
+    public class OrderDetail
+    {
+        [Key]
+        public long OrderDetailId { get; set; }
+        public long OrderMasterId { get; set; }
+        public int DessertItemId { get; set; }
+        public decimal DessetItemPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
