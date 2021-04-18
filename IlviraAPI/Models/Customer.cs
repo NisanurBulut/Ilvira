1dc918adc256b2aceca6503af52cc69c7ed155b5
NisanurBulut <nbulut@sarplojistik.com>
2021-04-18 14:03:59 +0300

    implemented customer model

---
+++ IlviraAPI/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IlviraAPI.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        [Column(TypeName ="nvarchar(100)")]
+        public string CustomerName { get; set; }
+    }
+}
