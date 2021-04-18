3c08f0d1951840d41a287b815dcd45b4ab0be32d
NisanurBulut <nbulut@sarplojistik.com>
2021-04-18 22:14:18 +0300

    implemented ilviradbcontext

---
+++ IlviraAPI/Models/IlviraDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IlviraAPI.Models
+{
+    public class IlviraDbContext : DbContext
+    {
+        public IlviraDbContext(DbContextOptions<IlviraDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> tCustomer { get; set; }
+        public DbSet<DessertItem> tDessertItem { get; set; }
+        public DbSet<OrderMaster> tOrderMaster { get; set; }
+        public DbSet<OrderMaster> tOrderDetail { get; set; }
+    }
+}
