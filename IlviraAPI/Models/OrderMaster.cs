5f7fd93129a12a9e338c2a1eb8a5c20ef152a6bc
NisanurBulut <nbulut@sarplojistik.com>
2021-04-18 14:44:45 +0300

    implemented OrderMaster model

---
+++ IlviraAPI/Models/OrderMaster.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IlviraAPI.Models
+{
+    public class OrderMaster
+    {
+        [Key]
+        public long OrderMasterId { get; set; }
+        [Column(TypeName ="nvarchar(75)")]
+        public string OrderNumber { get; set; }
+        public int CustomerId { get; set; }
+        [Column(TypeName = "nvarchar(15)")]
+        public string PaymentMethod { get; set; }
+        public decimal GTotal { get; set; }
+    }
+}
