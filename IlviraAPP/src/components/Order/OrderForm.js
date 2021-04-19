387e0c02b8118cd8d1bd30c013f0840b1d2f7502
NisanurBulut <nbulut@sarplojistik.com>
2021-04-19 22:40:26 +0300

    used Input custom control

--- IlviraAPP/src/components/Order/OrderForm.js
+++ IlviraAPP/src/components/Order/OrderForm.js
@@ -1,13 +1,17 @@
 import { Grid } from '@material-ui/core';
 import React from 'react';
 import Form from '../../layouts/Form';
-
+import Input from '../../controls/Input';
 export default function OrderForm() {
   return (
     <Form>
       <Grid container>
-          <Grid item xs={6}></Grid>
-          <Grid item xs={6}></Grid>
+        <Grid item xs={6}>
+          <Input disabled label="Order Number" name="orderNumber" />
+        </Grid>
+        <Grid item xs={6}>
+          <Input disabled label="Grand Total" name="gTotal" />
+        </Grid>
       </Grid>
     </Form>
   );
