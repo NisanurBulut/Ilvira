f0cdbb6863003995114f0c71069ed07239a30df2
NisanurBulut <nbulut@sarplojistik.com>
2021-04-19 21:59:19 +0300

    implemented form component

---
+++ IlviraAPP/src/layouts/Form.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiformControl-root': {
+      width: '90%',
+      margin: theme.spacing(1),
+    },
+  },
+}));
+export default function Form(props) {
+  const classes = useStyles();
+  const { children, ...other } = props;
+  return (
+    <form className={classes.root} noValidate autoComplete="off" {...other}>
+      {props.children}
+    </form>
+  );
+}
