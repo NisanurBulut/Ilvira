084105701de100a0b19ef8694776aa5b6ceeb2ca
NisanurBulut <nbulut@sarplojistik.com>
2021-04-19 22:30:53 +0300

    implemented input control

---
+++ IlviraAPP/src/controls/Input.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { TextField } from '@material-ui/core';
+export default function Input(props) {
+  const {
+    name,
+    label,
+    value,
+    variant,
+    onChange,
+    error = null,
+    ...other
+  } = props;
+  return (
+    <TextField
+      variant={variant || 'outlined'}
+      label={label}
+      name={name}
+      value={value}
+      onchange={onChange}
+      {...other}
+      {...(error && { error: true, helperText: error })}
+    ></TextField>
+  );
+}
