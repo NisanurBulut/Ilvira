7852ee0301d937d2c189c1083962e45cb08ca899
NisanurBulut <nbulut@sarplojistik.com>
2021-04-19 23:15:49 +0300

    implemented Select component

---
+++ IlviraAPP/src/controls/Select.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  FormControl,
+  InputLabel,
+  Select as MuiSelect,
+  MenuItem,
+  FormHelperText,
+} from '@material-ui/core';
+export default function Select(props) {
+  const {
+    name,
+    label,
+    value,
+    variant,
+    onChange,
+    options,
+    error = null,
+  } = props;
+  return (
+    <FormControl
+      variant={variant || 'outlined'}
+      {...(error && { error: true })}
+    >
+      <InputLabel>{label}</InputLabel>
+      <MuiSelect label={label} name={name} value={value} onChange={onChange}>
+        {options.map((item) => (
+          <MenuItem key={item.id} value={item.id}>
+            {item.title}
+          </MenuItem>
+        ))}
+      </MuiSelect>
+    </FormControl>
+  );
+}
