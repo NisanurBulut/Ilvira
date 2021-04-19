198b931a0950c18783c18ccbc0b0ac84651b0f2b
NisanurBulut <nbulut@sarplojistik.com>
2021-04-19 23:30:10 +0300

    used select control on orderform

--- IlviraAPP/src/components/Order/OrderForm.js
+++ IlviraAPP/src/components/Order/OrderForm.js
@@ -2,12 +2,22 @@ import { Grid } from '@material-ui/core';
 import React from 'react';
 import Form from '../../layouts/Form';
 import Input from '../../controls/Input';
+import Select from '../../controls/Select';
 export default function OrderForm() {
   return (
     <Form>
       <Grid container>
         <Grid item xs={6}>
           <Input disabled label="Order Number" name="orderNumber" />
+          <Select
+            label="Customer"
+            name="customerId"
+            options={[
+              { id: 1, title: 'Customer 1' },
+              { id: 2, title: 'Customer 2' },
+              { id: 3, title: 'Customer 3' },
+            ]}
+          ></Select>
         </Grid>
         <Grid item xs={6}>
           <Input disabled label="Grand Total" name="gTotal" />
