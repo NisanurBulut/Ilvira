48b681bc633b054032d2faa173afbb018f38f939
NisanurBulut <nbulut@sarplojistik.com>
2021-04-20 19:08:45 +0300

    implemented custom formhook

--- IlviraAPP/src/components/Order/OrderForm.js
+++ IlviraAPP/src/components/Order/OrderForm.js
@@ -1,43 +1,22 @@
 import { Grid } from '@material-ui/core';
-import React, { useState } from 'react';
+import React from 'react';
 import Form from '../../layouts/Form';
 import { Input, Select, Button } from '../../controls';
+
 
 const pMethods = [
   { id: 'none', title: 'Select' },
   { id: 'cash', title: 'Select' },
   { id: 'card', title: 'Select' },
 ];
-const generateOrderNumber = () =>
-  Math.floor(100000 + Math.random() * 900000).toString();
-const getFreshModelObject = () => ({
-  orderMasterId: 0,
-  orderNumber: generateOrderNumber(),
-  customerId: 0,
-  pMethod: 'none',
-  gTotal: 0,
-  deletedOrderItemIds: '',
-  orderDetails: [],
-});
-export default function OrderForm() {
-  const [values, setValues] = useState(getFreshModelObject());
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setValues({
-      ...values,
-      [name]: value,
-    });
-  };
 
-  const resetFormControls = () =>{
-    setValues();
-  }
-
+export default function OrderForm(props) {
+  const {values, errors, handleInputChange} = props;
   return (
     <>
       <Form>
-        <Grid container >
-          <Grid item xs={6} >
+        <Grid container>
+          <Grid item xs={6}>
             <Input
               disabled
               label="Order Number"
