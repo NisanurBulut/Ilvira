bb2520d4cad441bde531057fc7c53a2b120940d6
Nisanur BULUT <nbulut@sarplojistik.com>
2021-04-23 22:33:50 +0300

    created controllers for customer and dessertitem

---
+++ IlviraAPI/Controllers/CustomerController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IlviraAPI.Models;
+
+namespace IlviraAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IlviraDbContext _context;
+
+        public CustomerController(IlviraDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Customer
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GettCustomer()
+        {
+            return await _context.tCustomer.ToListAsync();
+        }
+
+        // GET: api/Customer/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+            var customer = await _context.tCustomer.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+
+        // PUT: api/Customer/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCustomer(int id, Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Customer
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+        {
+            _context.tCustomer.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCustomer", new { id = customer.CustomerId }, customer);
+        }
+
+        // DELETE: api/Customer/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            var customer = await _context.tCustomer.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.tCustomer.Remove(customer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.tCustomer.Any(e => e.CustomerId == id);
+        }
+    }
+}
---
+++ IlviraAPI/Controllers/DessertItemController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IlviraAPI.Models;
+
+namespace IlviraAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DessertItemController : ControllerBase
+    {
+        private readonly IlviraDbContext _context;
+
+        public DessertItemController(IlviraDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DessertItem
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DessertItem>>> GettDessertItem()
+        {
+            return await _context.tDessertItem.ToListAsync();
+        }
+
+        // GET: api/DessertItem/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DessertItem>> GetDessertItem(int id)
+        {
+            var dessertItem = await _context.tDessertItem.FindAsync(id);
+
+            if (dessertItem == null)
+            {
+                return NotFound();
+            }
+
+            return dessertItem;
+        }
+
+        // PUT: api/DessertItem/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDessertItem(int id, DessertItem dessertItem)
+        {
+            if (id != dessertItem.DessertItemId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(dessertItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DessertItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/DessertItem
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<DessertItem>> PostDessertItem(DessertItem dessertItem)
+        {
+            _context.tDessertItem.Add(dessertItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDessertItem", new { id = dessertItem.DessertItemId }, dessertItem);
+        }
+
+        // DELETE: api/DessertItem/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDessertItem(int id)
+        {
+            var dessertItem = await _context.tDessertItem.FindAsync(id);
+            if (dessertItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.tDessertItem.Remove(dessertItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DessertItemExists(int id)
+        {
+            return _context.tDessertItem.Any(e => e.DessertItemId == id);
+        }
+    }
+}
