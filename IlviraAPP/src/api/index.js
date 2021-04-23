1e5a160aa0b136e24903488e0c605342ab600102
Nisanur BULUT <nbulut@sarplojistik.com>
2021-04-23 22:42:51 +0300

    created service for api

---
+++ IlviraAPP/src/api/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const BASE_URL = "https://localhost:44314/api/";
+
+export const createAPIEndpoint = (endpoint) => {
+  let url = BASE_URL + endpoint + "/";
+  return {
+    fetchAll: () => axios.get(url),
+    fetchById: (id) => axios.get(url + id),
+    create: (newRecord) => axios.post(url, newRecord),
+    update: (id, updatedRecord) => axios.put(url + id, updatedRecord),
+    delete: (id) => axios.delete(url + id),
+  };
+};
