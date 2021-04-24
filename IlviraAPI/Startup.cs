55fab621bf8f1e31873361c7c0110b2cd838a826
Nisanur BULUT <nbulut@sarplojistik.com>
2021-04-24 12:51:55 +0300

    implemented cors side by backend

--- IlviraAPI/Startup.cs
+++ IlviraAPI/Startup.cs
@@ -33,6 +33,11 @@ namespace IlviraAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => 
+            options.WithOrigins("http://localhost:3000")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
