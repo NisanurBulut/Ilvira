82dc5cbae59c115a53cf6398a23cabd5d40149e0
Nisanur BULUT <nbulut@sarplojistik.com>
2021-04-24 13:42:57 +0300

    implemented search input with styles

--- IlviraAPP/src/components/Order/SearchDessertItems.js
+++ IlviraAPP/src/components/Order/SearchDessertItems.js
@@ -1,27 +1,58 @@
-import { List, ListItem, ListItemText } from "@material-ui/core";
+import {
+  IconButton,
+  InputBase,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
 import React, { useState, useEffect } from "react";
 import { ENDPOINTS, createAPIEndpoint } from "../../api";
+import SearchTwoTone from "@material-ui/icons/SearchTwoTone";
+import { Paper, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  searchPaper: {
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+  },
+  searchInput: {
+    marginLeft: theme.spacing(1.5),
+    flex: 1,
+  },
+}));
 
 export default function SearchDessertItems() {
   const [dessertItems, setDessertItems] = useState([]);
-
+  const classes = useStyles();
   useEffect(() => {
     createAPIEndpoint(ENDPOINTS.DESSERTITEM)
       .fetchAll()
       .then((res) => {
-          setDessertItems(res.data);
-          console.log(dessertItems);
+        setDessertItems(res.data);
+        console.log(dessertItems);
       })
       .catch((err) => console.log(err));
   }, []);
 
   return (
-    <List>
-      {dessertItems.map((item, index) =>(
+    <>
+      <Paper className={classes.searchPaper}>
+        <InputBase placeholder="Search desserts" className={classes.searchInput} />
+        <IconButton>
+          <SearchTwoTone />
+        </IconButton>
+      </Paper>
+      <List>
+        {dessertItems.map((item, index) => (
           <ListItem key={index}>
-          <ListItemText primary={item.dessertName} secondary={item.price+'₺'} />
-        </ListItem>
-      ))}
-    </List>
+            <ListItemText
+              primary={item.dessertName}
+              secondary={item.price + "₺"}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 }
