371e04a289c3676608f359877782cc6ca1b14215
Nisanur BULUT <nbulut@sarplojistik.com>
2021-04-25 21:01:08 +0300

    implemented style for header logo

--- IlviraAPP/src/App.js
+++ IlviraAPP/src/App.js
@@ -1,11 +1,36 @@
-import { Container, Typography } from "@material-ui/core";
+import { Container, Typography,Paper,makeStyles } from "@material-ui/core";
 import './App.css';
 import Order from "./components/Order";
+import ilviraImage from './images/ilvira.png';
+
+const useStyles = makeStyles((theme) => ({
+  rootPaper: {
+    padding: "8px 68px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent:"center"
+  },
+  ilviraImage:{
+    borderRadius:"5px",
+    width:"90px",
+    height:"90px",
+    marginRight:"2em"
+  },
+  ilviraText:{
+    color:"#D4389A",
+    fontStyle:"italic",
+    textDecorationLine: 'underline'
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <Container maxWidth="md">
-      <Typography gutterBottom variant="h2" align="center">Ilvira</Typography>
+      <div className={classes.rootPaper}>
+      <img className={classes.ilviraImage} src={ilviraImage}/> 
+      <Typography className={classes.ilviraText} gutterBottom variant="h2">Ilvira</Typography>
+      </div>
       <Order />
     </Container>
   );
